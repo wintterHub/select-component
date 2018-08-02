8b264d9a0be67acb89dd34075d7aa90f8fc4ebee
zhaochangjin <zhaochangjin@zving.com>
2018-08-02 20:26:32 +0800

    添加异步请求

--- select.vue
+++ select.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <el-select v-model="value9" multiple filterable remote reserve-keyword placeholder="请输入关键词" :remote-method="remoteMethod"
+    <el-select v-model="vModel" multiple filterable remote reserve-keyword placeholder="placeholder" :remote-method="remoteMethod"
       :loading="loading">
-      <el-option v-for="item in options4" :key="item.value" :label="item.label" :value="item.value">
+      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
       </el-option>
     </el-select>
   </div>
@@ -10,49 +10,41 @@
 
 <script>
   export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '请输入关键词',
+        required: true,
+      },
+      vModel: {
+        type: String,
+        required: true,
+      },
+      action: {//选项值异步请求地址
+        type: String,
+        required: true,
+      }
+    },
     data() {
       return {
-        options4: [],
-        value9: [],
+        options: [],
         list: [],
-        loading: false,
-        states: ["Alabama", "Alaska", "Arizona",
-          "Arkansas", "California", "Colorado",
-          "Connecticut", "Delaware", "Florida",
-          "Georgia", "Hawaii", "Idaho", "Illinois",
-          "Indiana", "Iowa", "Kansas", "Kentucky",
-          "Louisiana", "Maine", "Maryland",
-          "Massachusetts", "Michigan", "Minnesota",
-          "Mississippi", "Missouri", "Montana",
-          "Nebraska", "Nevada", "New Hampshire",
-          "New Jersey", "New Mexico", "New York",
-          "North Carolina", "North Dakota", "Ohio",
-          "Oklahoma", "Oregon", "Pennsylvania",
-          "Rhode Island", "South Carolina",
-          "South Dakota", "Tennessee", "Texas",
-          "Utah", "Vermont", "Virginia",
-          "Washington", "West Virginia", "Wisconsin",
-          "Wyoming"]
+        loading: false
       }
     },
     mounted() {
-      this.list = this.states.map(item => {
-        return { value: item, label: item };
-      });
+
     },
     methods: {
-      remoteMethod(query) {
+      async remoteMethod(query) {
         if (query !== '') {
           this.loading = true;
           setTimeout(() => {
             this.loading = false;
-            this.options4 = this.list.filter(item => {
-              return item.label.toLowerCase()
-                .indexOf(query.toLowerCase()) > -1;
-            });
+            this.options = await axios(`${this.action}`, { value: vModel }).data.data;
           }, 200);
         } else {
-          this.options4 = [];
+          this.options = [];
         }
       }
     }
